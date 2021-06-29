733377cf5e20b27b5639cf7cc45422f09fa0ecff
mimiring <island_2@naver.com>
2021-06-29 16:47:00 +0900

    프로그래머스 레벨1 연습 - 약수의 갯수와 덧셈

---
+++ 약수의개수와덧셈.js
@@ -0,0 +1,23 @@
+function solution(left, right) {
+  let answer = 0;
+  let calcul = [];
+
+  for (let i = left; i <= right; i++) {
+    let count = 0;
+
+    for (let j = 1; j <= i; j++) {
+      if (i % j === 0) {
+        count++;
+      }
+    }
+    if (count % 2 === 0) {
+      answer += i;
+    } else {
+      answer -= i;
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution(13, 17));
